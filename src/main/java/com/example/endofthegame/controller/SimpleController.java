879e4d82e78601c76726bacc71d9c0a1e328b04a
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-10-22 12:02:39 +0300

    [add] - simple rest example implemented

---
+++ src/main/java/com/example/endofthegame/controller/SimpleController.java
@@ -0,0 +1,21 @@
+package com.example.endofthegame.controller;
+
+
+import com.example.endofthegame.entity.Address;
+import com.example.endofthegame.entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
+
+    @GetMapping("/me")
+    public Person showMyData() {
+        log.info("showing my personal data");
+        return new Person("Hindrek", "Ülemaantee", 30, new Address("Estonia", "Tallinn"));
+    }
+}
