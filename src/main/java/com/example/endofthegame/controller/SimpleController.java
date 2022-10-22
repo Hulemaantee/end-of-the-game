8fc0c80248935a5a6e1bd423673c5409d06164f1
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-10-22 13:21:47 +0300

    [add] - documentation started + some exercises with REST

--- src/main/java/com/example/endofthegame/controller/SimpleController.java
+++ src/main/java/com/example/endofthegame/controller/SimpleController.java
@@ -6,6 +6,7 @@ import com.example.endofthegame.entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +14,15 @@ public class SimpleController {
 
     private static final Logger log = LoggerFactory.getLogger(SimpleController.class);
 
+    // /me?my-name=Mariusz&my-surname=P.
     @GetMapping("/me")
-    public Person showMyData() {
+    public Person showMyData(@RequestParam(value = "my-name", defaultValue = "John") String myName, //required = false
+                             @RequestParam(value = "my-surname", defaultValue = "Smith") String mySurname) {
         log.info("showing my personal data");
-        return new Person("Hindrek", "Ülemaantee", 30, new Address("Estonia", "Tallinn"));
+        log.info("received name: [{}], and surname: [{}]", myName, mySurname);
+
+        return new Person(myName, mySurname, 18,
+                new Address("Estonia", "Tallinn"));
     }
 }
+
