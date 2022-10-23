2cb3a7a47c155e7d5cbdf8cfb0f2b68b6bf58443
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-10-23 13:59:32 +0300

    [change] - reading all incomes implemented

--- src/main/java/com/example/endofthegame/entity/Income.java
+++ src/main/java/com/example/endofthegame/entity/Income.java
@@ -1,18 +1,30 @@
 package com.example.endofthegame.entity;
 
 import com.example.endofthegame.entity.enumeration.IncomeCategory;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
-public record Income(
-        IncomeCategory category,
-        String currency,
-        BigDecimal amount,
-        String person, // TODO: Refactor later
-        ZonedDateTime timestamp
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Income {
+        // TODO: improve generation of ID
+        @Id
+        Long id;
+        IncomeCategory category;
+        String currency;
+        BigDecimal amount;
+        String person; // TODO: Refactor later
+        ZonedDateTime timestamp;
         // String source,
         // String type
-
-) {
 }
