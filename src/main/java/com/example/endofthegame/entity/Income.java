a74107404e73c504ae7f5446dd18bdaa2d3b8768
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-10-23 12:02:57 +0300

    [add] - data model for incomes

---
+++ src/main/java/com/example/endofthegame/entity/Income.java
@@ -0,0 +1,18 @@
+package com.example.endofthegame.entity;
+
+import com.example.endofthegame.entity.enumeration.IncomeCategory;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+public record Income(
+        IncomeCategory category,
+        String currency,
+        BigDecimal amount,
+        String person, // TODO: Refactor later
+        ZonedDateTime timestamp
+        // String source,
+        // String type
+
+) {
+}
