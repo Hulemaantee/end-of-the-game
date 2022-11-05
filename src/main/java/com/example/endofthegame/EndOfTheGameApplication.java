e85704d3ce8d42be3ad44d4b168258bb73f0d209
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-11-05 10:25:13 +0200

    [add] - separate main class for homework

--- src/main/java/com/example/endofthegame/EndOfTheGameApplication.java
+++ src/main/java/com/example/endofthegame/EndOfTheGameApplication.java
@@ -1,9 +1,7 @@
 package com.example.endofthegame;
 
-import com.example.homework.exercise1.Calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EndOfTheGameApplication {
@@ -12,10 +10,11 @@ public class EndOfTheGameApplication {
 		SpringApplication.run(EndOfTheGameApplication.class, args);
 	}
 
-	@Bean
+	// use @Bean for direct creation of spring beans
+/*	@Bean
 	public Calculator calculator() {
 		return new Calculator();
-	}
+	}*/
 
 
 }
