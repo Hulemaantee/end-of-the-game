c125b56834a6703895e361a74db48e1c6ae5c2bb
Hindrek Ülemaantee <hindrek.ulemaantee@gmail.com>
2022-11-20 11:37:05 +0200

    [add] - 404 page created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IncomeListComponent } from './components/income-list/income-list.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
 import {RouterModule} from "@angular/router";
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     IncomeListComponent,
-    ExpenseListComponent
+    ExpenseListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -23,10 +26,12 @@ import {RouterModule} from "@angular/router";
     MatCardModule,
     MatButtonModule,
     RouterModule.forRoot([
-      {path:'', component: IncomeListComponent},
+      {path: '', component: IncomeListComponent},
       {path: 'income-list', component: IncomeListComponent},
-      {path: 'expense-list', component: ExpenseListComponent}
-    ])
+      {path: 'expense-list', component: ExpenseListComponent},
+      {path: '**', component: NotFoundComponent}
+    ]),
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
